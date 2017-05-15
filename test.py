41c49eae27bd633398f19be8516e0398804e3128
elij <elij@wundrd.net>
2017-05-15 11:52:42 -0700

    add man page

--- test.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sys.stdout.write("test-before\n")
-sys.stdout.flush()
-sys.stdout.write("fail"*1024*65)
-sys.stdout.write("\n")
-sys.stdout.flush()
-sys.stdout.write("test-after\n")
-sys.stdout.flush()
